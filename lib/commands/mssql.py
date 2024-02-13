b48d5391a57dd574830074ec58a1e4f71b5bbf32
Garrett Foster <garrfoster@gmail.com>
2024-02-12 22:52:07 -0800

    updated MSSQL to use individual or stacked queries

--- lib/commands/mssql.py
+++ lib/commands/mssql.py
@@ -20,12 +20,13 @@ def main(
     aes             : str   = typer.Option(None, '-aes', metavar="HEX KEY", help='AES key to use for Kerberos Authentication (128 or 256 bits)'), 
     debug           : bool  = typer.Option(False, '-debug',help='Enable Verbose Logging'),
     target_user     : str   = typer.Option(..., '-tu', help="Controlled user to grant permissions to."),
+    stacked         : bool  = typer.Option(False, '-stacked', help="Provide a single stacked query for relaying."),
     site_code       : str   = typer.Option(..., '-sc', help="Target site code to add user to.")):
 
 
     mssqlhunter = MSSQL(username=username, password=password, domain=domain, dc_ip=dc_ip,ldaps=ldaps,
                             kerberos=kerberos, no_pass=no_pass, hashes=hashes, aes=aes, debug=debug, 
-                            target_user=target_user, site_code=site_code)
+                            target_user=target_user, stacked=stacked, site_code=site_code)
     init_logger(debug)
     mssqlhunter.run()
 
