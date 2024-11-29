874ffbc98c40f8213218a24b6dbac9783870a65a
Adamkadaban <adamkadaban@gmail.com>
2024-11-29 18:06:56 -0500

    requirements.txt installation so sccmhunter can be installed with pipx

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup
 
+with open('requirements.txt', 'r', encoding='utf-8') as fin:
+  requirements = [i.strip() for i in fin.readlines()]
+
 setup(
   name='sccmhunter',
+  install_requires=requirements,
   scripts = ['sccmhunter.py'],
 )
